75dee943dd940fe1c1faabe4f933f85c9890ced8
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-03-01 21:11:58 +0100

    Add DB setup

---
+++ UserDb/Script.PostDeployment.sql
@@ -0,0 +1,10 @@
+﻿if not exists(SELECT 1 FROM dbo.[User])
+begin
+	INSERT 
+	INTO db.[User] (FirstName, LastName) 
+	VALUES 
+	('John', 'Snow'),
+	('Mike', 'Magic'),
+	('Sam', 'Smith'),
+	('Anna', 'Johnes');
+end
---
+++ UserDb/dbo/StoredProcedures/spUser_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetAll]
+	@param1 int = 0,
+	@param2 int
+AS
+begin
+	SELECT Id, FirstName, LastName 
+	FROM dbo.[User];
+end
